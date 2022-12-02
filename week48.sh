e1fd71b606490371b145444bc50b7f1123d67335
Donald Shtjefni <dnld.sht@gmail.com>
2022-12-02 17:55:37 +0100

    test: week 48

--- week48.sh
+++ week48.sh
@@ -1,12 +1,19 @@
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 89  -results results/week48/GRU128-LR6-BS256/seed89
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 89  -results results/week48/GRU128-LR6-BS32/seed89
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 89  -results results/week48/GRU128-G5-LR6-BS256/seed89
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 89  -results results/week48/GRU128-G5-LR6-BS32/seed89
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 196  -results results/week48/GRU128-G5-LR6-BS256/seed196
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 196  -results results/week48/GRU128-G5-LR6-BS32/seed196
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 23  -results results/week48/GRU128-G5-LR6-BS256/seed23
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 23  -results results/week48/GRU128-G5-LR6-BS32/seed23
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 27836  -results results/week48/GRU128-G5-LR6-BS256/seed27836
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 27836  -results results/week48/GRU128-G5-LR6-BS32/seed27836
 
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 196  -results results/week48/GRU128-LR6-BS256/seed196
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 196  -results results/week48/GRU128-LR6-BS32/seed196
 
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 23  -results results/week48/GRU128-LR6-BS256/seed23
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 23  -results results/week48/GRU128-LR6-BS32/seed23
-
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 27836  -results results/week48/GRU128-LR6-BS256/seed27836
-python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 27836  -results results/week48/GRU128-LR6-BS32/seed27836
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 89  -results results/week48/GRU128-G1-LR6-BS256/seed89
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 89  -results results/week48/GRU128-G1-LR6-BS32/seed89
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 196  -results results/week48/GRU128-G1-LR6-BS256/seed196
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 196  -results results/week48/GRU128-G1-LR6-BS32/seed196
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 23  -results results/week48/GRU128-G1-LR6-BS256/seed23
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 23  -results results/week48/GRU128-G1-LR6-BS32/seed23
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 256 --seed 27836  -results results/week48/GRU128-G1-LR6-BS256/seed27836
+python train.py --epoch 600 --lamda_D 1 --G_epoch 5 --cell_type GRU --hidden_size 128 --learning_rate 1e-6 --batch_size 32 --seed 27836  -results results/week48/GRU128-G1-LR6-BS32/seed27836
 
